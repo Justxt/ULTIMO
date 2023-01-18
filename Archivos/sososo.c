6d00b4a7cbc6a2e87fb4084317b035da6aa526ca
Justin Mora <96129728+Justxt@users.noreply.github.com>
2023-01-18 13:49:20 -0500

    se arreglo el archivo2 xd

--- Archivos/sososo.c
+++ Archivos/sososo.c
@@ -2,25 +2,30 @@
 
 int main(){
 
-    FILE *vector=NULL;
+    FILE *archivo=NULL;
+    FILE *archivo2=NULL;
     char buffer[100];
     double pi;
+    int res, mul;
 
-    vector=fopen("vector.txt","r+");
+    archivo=fopen("vector.txt","r+");
+    archivo2=fopen("vector1.txt","w");
 
-    vector=fopen("vector1.txt","w");
-
-        if (vector==NULL){
+        if (archivo==NULL){
+            if (archivo==NULL){
         printf("No se puede abrir el archivo");
         return -1;
     }
+}
 
-    int numero = 3;
         for(int i=0; i<100; i++){
-        fprintf(vector,"%d\n",i * numero);
+            fscanf(archivo, "%d", &mul);
+        res = mul * 3;
+        fprintf(archivo2,"%d\n",res);
 
     }
 
-     fclose(vector);
+     fclose(archivo);
+     fclose(archivo2);
      return 0;
 }
